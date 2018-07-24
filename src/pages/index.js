352b4003137a306d862e1af8a6815634de8a4967
Miguel Palau Zarza <mpalauzarza@gmail.com>
2018-07-23 19:55:06 -0500

    moving to gatsby v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import Img from 'gatsby-image';
+import Layout from '../components/layout'
 
-const IndexPage = ({ data }) =>
-  <div>
-
-    <Img sizes={data.headerImage.sizes} />
-
-    <p>Hi there! Thanks for stopping by 👋</p>
+const IndexPage = () =>
+  <Layout>
+    <p>Hi there! Thanks for stopping by <span role='img' aria-label='Hand waving hello'>👋</span></p>
     <p>My name is Miguel Palau I'm a Web Developer from <a href="https://en.wikipedia.org/wiki/Puebla">Puebla, México</a></p>
     <p>I love working with modern web technologies and I'm passionate about always staying sharp on my skills and learning new stuff all the time.</p>
     <p>Also I enjoy working with and contributing to <a href="https://github.com/mike3run/">open source</a> and also I love tweaking my terminal and <a href="https://github.com/mike3run/dotfiles">dotfiles</a></p>
@@ -15,17 +11,6 @@ const IndexPage = ({ data }) =>
     <p>I organize a monthly Meetup in Puebla called <a href="https://www.meetup.com/es-ES/DevNightsMX/">DevNights</a> were we talk about open source technologies, eat some 🍕 and have a few 🍺. If you're around the area you should probably go 💁.</p>
     <p>Ocassionally I <a href="https://twitter.com/mpalau">tweet</a> in a mix of spanish and english and write on <a href="https://www.pixel2html.com/blog/">Pixel's Blog</a> and I'm also trying to blog more often here as well.</p>
     <p>This webpage was built using <a href="https://phenomic.io/">Phenomic</a> and its hosted on <a href="https://pages.github.com/">Github Pages</a> feel free to check the <a href="https://github.com/mike3run/imike.pw">source code</a></p>
-  </div>
+  </Layout>
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query HeaderImageQuery {
-    headerImage: imageSharp(id: { regex: "/sunset/" }) {
-      sizes(maxWidth: 1240 ) {
-        ...GatsbyImageSharpSizes
-        ## ...GatsbyImageSharpSizes_tracedSVG
-      }
-    }
-  }
-`
